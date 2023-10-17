b0636e96d109494b3afb1680bf7b6ce3a637c904
Jordan-Conway <D00244954@student.dkit.ie>
2023-10-17 20:25:58 +0100

    Add project files.

---
+++ TestCA2/TestCA2.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestCA2
+{
+	TEST_CLASS(TestCA2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
