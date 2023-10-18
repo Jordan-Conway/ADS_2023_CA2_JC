c4d0634f4a1c43f2fdcfd39bad7170ad9420e0d6
Jordan-Conway <D00244954@student.dkit.ie>
2023-10-18 23:10:58 +0100

    Add TagTypeToString method

--- CA2/TagType.h
+++ CA2/TagType.h
@@ -1,7 +1,17 @@
 #pragma once
 
-enum TagType {
+enum class TagType {
 	OPENING,
 	CLOSING,
 	SELFCLOSING
 };
+
+std::string TagTypeToString(TagType tagType) 
+{
+	switch (tagType)
+	{
+		case TagType::OPENING: return "OPENING";
+		case TagType::CLOSING: return "CLOSING";
+		case TagType::SELFCLOSING: return "SELFCLOSING";
+	}
+}
