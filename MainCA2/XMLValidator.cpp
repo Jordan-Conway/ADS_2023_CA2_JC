c4c732213b3fd3017652b94640ab3368d20b1fa8
Jordan-Conway <D00244954@student.dkit.ie>
2023-11-30 13:09:14 +0000

    Seperate Validation and Tree Creation

--- MainCA2/XMLValidator.cpp
+++ MainCA2/XMLValidator.cpp
@@ -1,11 +1,14 @@
 #include "XMLValidator.h"
 #include "XMLTagParser.h"
 #include "TreeIterator.h"
+#include <queue>
+#include <set>
 #include <list>
 
 std::optional<Tree<Tag>> XMLValidator::validate(std::ifstream& stream)
 {
     std::string line;
+    std::queue<Tag> tagQueue;
     XMLTagParser tagParser;
 
     std::list<Tag> tags;
@@ -32,56 +35,114 @@ std::optional<Tree<Tag>> XMLValidator::validate(std::ifstream& stream)
     std::stack<Tag> stack;
 
     //Add the first tag
-    Tree<Tag>* tree = new Tree(tags.front());
-    TreeIterator<Tag> iter(tree);
+    tagQueue.push(tags.front());
     stack.push(tags.front());
     tags.pop_front();
 
+    bool linesRemaining = true;
+
     //Process the rest of the file
-    while (stream.peek() != std::ifstream::traits_type::eof())
+    while (linesRemaining)
     {
-        if (!tags.empty())
+        if (stream.peek() == std::ifstream::traits_type::eof())
+        {
+            linesRemaining = false;
+        }
+
+        while (!tags.empty())
         {
             switch (tags.front().getTagType())
             {
-            case OPENING:
-            {
-                stack.push(tags.front());
-                iter.appendChild(tags.front());
-                iter.childIter.end();
-                iter.down();
-                tags.pop_front();
-                break;
+                case OPENING:
+                {
+                    stack.push(tags.front());
+                    break;
+                }
+
+                case CLOSING:
+                {
+                    //If tag is now closed
+                    if (tags.front().closes(stack.top()))
+                    {
+                        stack.pop();
+                    }
+                    //The document is invalid
+                    else
+                    {
+                        return {};
+                    }
+                    break;
+                }
             }
 
-            case CLOSING:
-            {
-                //If tag is now closed
-                if (tags.front().closes(stack.top()))
-                {
-                    iter.up();
-                    stack.pop();
-                }
-                //The document is invalid
-                else
-                {
-                    delete tree;
-                    return {};
-                }
-                break;
-            }
-
-            case SELFCLOSING:
-            {
-                iter.appendChild(tags.front());
-                tags.pop_front();
-                break;
-            }
-            }
+            tagQueue.push(tags.front());
+            tags.pop_front();
         }
 
         std::getline(stream, line);
         tags = tagParser.getTagsFromString(line);
+    }
+
+    if (!stack.empty())
+    {
+        return {};
+    }
+
+    if (tagQueue.empty())
+    {
+        return {};
+    }
+
+    Tree<Tag>* tree = new Tree(tagQueue.front());
+    TreeIterator<Tag> iter(tree);
+    tagQueue.pop();
+
+    std::set<std::string> nodeTags;
+    nodeTags.insert("root");
+    nodeTags.insert("dir");
+    nodeTags.insert("file");
+
+    std::set<std::string, std::string> propertTags;
+    nodeTags.insert("name");
+    nodeTags.insert("length");
+    nodeTags.insert("config");
+
+    while (!tagQueue.empty())
+    {
+        if (nodeTags.find(tagQueue.front().getTagName()) != nodeTags.end())
+        {
+            switch (tagQueue.front().getTagType())
+            {
+                case OPENING:
+                {
+                    iter.appendChild(tagQueue.front());
+                    while (iter.childIter.currentNode->data->getData() != tagQueue.front())
+                    {
+                        iter.childIter.advance();
+                    }
+                    iter.down();
+                    break;
+                }
+
+                case CLOSING:
+                {
+                    iter.up();
+                    break;
+                }
+
+                case SELFCLOSING:
+                {
+                    iter.appendChild(tagQueue.front());
+                    while (iter.childIter.currentNode->data->data != tagQueue.front())
+                    {
+                        iter.childIter.advance();
+                    }
+                    break;
+                }
+            }
+        }
+
+        tagQueue.pop();
     }
 
     return *tree;
