449b815fc75d4c5deda70e65c38391dbe6ce506f
Jordan-Conway <D00244954@student.dkit.ie>
2023-12-06 22:52:44 +0000

    Add start of console application

--- MainCA2/main.cpp
+++ MainCA2/main.cpp
@@ -1,7 +1,70 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <windows.h>
+#include <fstream>
+#include <optional>
+#include <filesystem>
+#include <chrono>
+#include <thread>
+#include "XMLValidator.h"
+#include "Tree.h"
+#include "Tag.h"
+
+using namespace std;
+
+static string getInput();
+static void displayHelp();
+static void exitProgram();
+
+chrono::seconds exitDelay(1);
 
 int main() {
-    std::cout << "Hello World" << "\n";
+    cout << "Type help to see all commands" << "\n";
+    bool exit = false;
 
+    while (!exit)
+    {
+        cout << "Enter a command: ";
+        string input = getInput();
+        cout << "\n";
+
+        if (input == "help")
+        {
+            displayHelp();
+            continue;
+        }
+
+        if (input == "exit")
+        {
+            exitProgram();
+        }
+
+    }
+    
     return 0;
+}
+
+static string getInput()
+{
+    string input = "";
+
+    cin >> input;
+
+    //Convert input to lower case
+    transform(input.begin(), input.end(), input.begin(), ::tolower);
+
+    return input;
+}
+
+static void displayHelp()
+{
+    cout << "help: show all commands" << "\n";
+    cout << "exit: exit the program" << "\n";
+}
+
+static void exitProgram()
+{
+    cout << "Thank you for using this program" << "\n";
+    this_thread::sleep_for(exitDelay);
+    std::exit(0);
 }
