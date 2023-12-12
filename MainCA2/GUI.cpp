8f2d853be627fe1d1b2c60fc327b9ab3254e7192
Jordan-Conway <D00244954@student.dkit.ie>
2023-12-12 10:15:34 +0000

    Add GUI

--- MainCA2/GUI.cpp
+++ MainCA2/GUI.cpp
@@ -1,8 +1,10 @@
 #include "GUI.h"
+#include <iostream>
 
 GUI::GUI()
 {
-    this->window = new sf::Window(sf::VideoMode(800, 600), "Tree");
+    this->window = new sf::RenderWindow(sf::VideoMode(1280, 720), "Tree");
+    window->setFramerateLimit(24);
     tree = nullptr;
 }
 
@@ -11,7 +13,58 @@ GUI::~GUI()
     delete this->window;
 }
 
+void GUI::display()
+{
+    if (this->tree == nullptr)
+    {
+        return;
+    }
+
+    while (window->isOpen())
+    {
+        window->clear();
+        sf::Event event;
+        while (window->pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window->close();
+            }
+        }
+        this->drawTree(this->tree);
+
+        window->display();
+    }
+}
+
 void GUI::loadTree(Tree<Tag>* root)
 {
     this->tree = root;
+}
+
+int GUI::drawTree(Tree<Tag>* tree, int indent, int layer)
+{
+    int numDrawn = 1;
+    sf::Font font;
+    if (!font.loadFromFile("arial.ttf")) 
+    {
+        std::cout << "Failed to load font" << "\n";
+        return 0;
+    }
+    sf::Text text;
+    text.setString(tree->data.getName());
+    text.setCharacterSize(10);
+    text.setFillColor(sf::Color::White);
+    text.setPosition(20 + (indent * 20), 20 + (layer * 20));
+    text.setFont(font);
+    window->draw(text);
+
+    DListIterator<Tree<Tag>*> iter = tree->children->getIterator();
+    while (iter.isValid())
+    {
+        numDrawn += drawTree(iter.currentNode->data, indent + 1, layer + numDrawn);
+        iter.advance();
+    }
+
+    return numDrawn;
 }
--- MainCA2/GUI.h
+++ MainCA2/GUI.h
@@ -1,16 +1,18 @@
 #pragma once
 #include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
 #include "Tree.h"
 #include "Tag.h"
 
 class GUI
 {
-    sf::Window* window;
+    sf::RenderWindow* window;
     Tree<Tag>* tree;
-
+    int drawTree(Tree<Tag>* tree, int indent = 0, int layer = 0);
 public:
     GUI();
     ~GUI();
+    void display();
     void loadTree(Tree<Tag>* root);
 };
 
